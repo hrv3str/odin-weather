0474cafdcedacd1d0d34d607ea1970589a588f6b
Roman Novikov <r.novikov.zp.ua@gmail.com>
2023-09-15 14:30:01 +0300

    Merge weather.get and weather.locate functions for a better results

--- src/weather.js
+++ src/weather.js
@@ -1,28 +1,8 @@
 const weather = (() => {
-    let location = 'new_york' //Stores the loacation
+    let location = '' //Stores the location
+    const get = async (input) => { //Fetches weather data from weather API
+        const string = input;
 
-    const get = async () => { //Fetches weather data from weather API
-        if (location === 'location_error' //Breaks function if location is not valid
-            || location === '') {
-                console.error(`weather.get - location is not valid, ${location}`)
-            return;
-        }
-
-        const weatherAPICall = `https://api.weatherapi.com/v1/forecast.json?key=ed8fce714bb34831a25112542230709&q=${location}&days=1&aqi=no&alerts=no`;
-        const response = await fetch(weatherAPICall, {
-            mode: 'cors'
-        });
-
-        if (response.ok) {
-            const data = await response.json();
-            console.log(`weather.get - got weather data on ${location}`);
-            return data;
-        } else {
-            console.error('WeatherAPI response is not OK!')
-        }
-    }
-
-    const locate = async (input) => { //Takes user input, validates it and returns the location
         const validate = async (input) => {
             const geoAPICall = `http://api.geonames.org/searchJSON?q=${encodeURIComponent(input)}&maxRows=10&username=novikadze`;
             const response = await fetch(geoAPICall,  {
@@ -42,16 +22,39 @@ const weather = (() => {
                 console.error('GeoNames response is not OK!')
             }
         }
-        
-        location =  validate(input)
-            .toLowerCase()
-            .trim()
-            .replace(/\s+/g,'_');
+
+        await validate(string).then((result) => {
+            location = result.toLowerCase()
+                .trim()
+                .replace(/\s+/g,'_');
+        });
+
+        if (location === 'location_error' //Breaks function if location is not valid
+            || location === '') {
+                throw new Error(`weather.get - location is not valid, ${location}`);
+        }
+
+        const weatherAPICall = `https://api.weatherapi.com/v1/forecast.json?key=ed8fce714bb34831a25112542230709&q=${location}&days=1&aqi=no&alerts=no`;
+        const response = await fetch(weatherAPICall, {
+            mode: 'cors'
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            console.log(`weather.get - got weather data on ${location}`);
+            return data;
+        } else {
+            console.error('WeatherAPI response is not OK!')
+        }
+    }
+
+    const shareLocation = () => {
+        return location;
     }
 
     return {
         get,
-        locate,
+        shareLocation,
     }
 })();
 
